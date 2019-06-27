98c1199e3a25fe0cf7bbc9f6ccf2593c456d0803
GUPTA, AKSHAT <Akshat_Gupta@intuit.com>
2019-06-27 15:04:38 +0530

    Initial Commit

---
+++ QB-marketAnalysis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QB-marketAnalysis
+//
+//  Created by GUPTA, AKSHAT on 27/06/19.
+//  Copyright © 2019 GUPTA, AKSHAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
